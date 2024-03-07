cf9fd8a5e1e614bf377e9e8d9d5f852ab52f74f3
Aline <1231866@isep.ipp.pt>
2024-03-07 22:58:28 +0000

    [JobYears] - Add tests to JobYears. Closes #3

--- CA1/tutorial/basic/src/test/com/gregIturnquist/payroll/EmployeeTests.java
+++ CA1/tutorial/basic/src/test/com/gregIturnquist/payroll/EmployeeTests.java
@@ -1,17 +1,23 @@
-package test.com.gregIturnquist.payroll;
+package com.gregIturnquist.payroll;
 
 import com.greglturnquist.payroll.Employee;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootConfiguration
 class EmployeeTest {
     Employee emp;
+    String CONSTRUCTOR_ERROR_MESSAGE = "";
+    String JOB_YEARS_ERROR_MESSAGE = "";
+
     @BeforeEach
     public void setup() {
-        emp = new Employee("Frodo", "Baggins", "ring bearer", "Hobbit");
+        emp = new Employee("Frodo", "Baggins", "ring bearer", "Hobbit", 2);
+        CONSTRUCTOR_ERROR_MESSAGE = "Arguments must not be null or empty.";
+        JOB_YEARS_ERROR_MESSAGE = "Job years must be a positive number.";
     }
 
     @Test
@@ -30,6 +36,22 @@ class EmployeeTest {
     }
 
     @Test
+    public void invalidEmployeeFirstName() {
+        Exception e = assertThrows(IllegalArgumentException.class,
+              () -> new Employee("", "Baggins",
+                    "ring bearer", "Hobbit",2));
+        assertEquals(CONSTRUCTOR_ERROR_MESSAGE, e.getMessage());
+    }
+
+    @Test
+    public void invalidEmployeeFirstNameNull() {
+        Exception e = assertThrows(IllegalArgumentException.class,
+              () -> new Employee(null, "Baggins",
+                    "ring bearer", "Hobbit",2));
+        assertEquals(CONSTRUCTOR_ERROR_MESSAGE, e.getMessage());
+    }
+
+    @Test
     public void testEmployeeLastName() {
         String expected = "Baggins";
         String actual = emp.getLastName();
@@ -42,6 +64,22 @@ class EmployeeTest {
         emp.setLastName("Lastname");
         String actual = emp.getLastName();
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void invalidEmployeeLastName() {
+        Exception e = assertThrows(IllegalArgumentException.class,
+              () -> new Employee("Frodo", "",
+                    "ring bearer", "Hobbit",2));
+        assertEquals(CONSTRUCTOR_ERROR_MESSAGE, e.getMessage());
+    }
+
+    @Test
+    public void invalidEmployeeLastNameNull() {
+        Exception e = assertThrows(IllegalArgumentException.class,
+              () -> new Employee("Frodo", null,
+                    "ring bearer", "Hobbit",2));
+        assertEquals(CONSTRUCTOR_ERROR_MESSAGE, e.getMessage());
     }
 
     @Test
@@ -60,6 +98,22 @@ class EmployeeTest {
     }
 
     @Test
+    public void invalidEmployeeDescription() {
+        Exception e = assertThrows(IllegalArgumentException.class,
+              () -> new Employee("Frodo", "Baggins",
+                    "", "Hobbit",2));
+        assertEquals(CONSTRUCTOR_ERROR_MESSAGE, e.getMessage());
+    }
+
+    @Test
+    public void invalidEmployeeDescriptionNull() {
+        Exception e = assertThrows(IllegalArgumentException.class,
+              () -> new Employee("Frodo", "Baggins",
+                    null, "Hobbit",2));
+        assertEquals(CONSTRUCTOR_ERROR_MESSAGE, e.getMessage());
+    }
+
+    @Test
     public void testEmployeeJobTitle() {
         String expected = "Hobbit";
         String actual = emp.getJobTitle();
@@ -75,10 +129,62 @@ class EmployeeTest {
     }
 
     @Test
+    public void invalidEmployeeJobTitle() {
+        Exception e = assertThrows(IllegalArgumentException.class,
+              () -> new Employee("Frodo", "Baggins",
+                    "ring bearer", "",2));
+        assertEquals(CONSTRUCTOR_ERROR_MESSAGE, e.getMessage());
+    }
+
+    @Test
+    public void invalidEmployeeJobTitleNull() {
+        Exception e = assertThrows(IllegalArgumentException.class,
+              () -> new Employee("Frodo", "Baggins",
+                    "ring bearer", null,1));
+        assertEquals(CONSTRUCTOR_ERROR_MESSAGE, e.getMessage());
+    }
+
+    @Test
     public void testToString() {
         String expected = "firstName='Frodo', lastName='Baggins'";
         String actual = emp.toString();
         assertTrue(actual.contains(expected));
     }
 
+    @Test
+    public void testEmployeeJobYears() {
+        int expected = 2;
+        int actual = emp.getJobYears();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testEmployeeJobYearsAfterSetJobYears() {
+        int expected = 3;
+        emp.setJobYears(3);
+        int actual = emp.getJobYears();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void invalidEmployeeJobYears() {
+        Exception e = assertThrows(IllegalArgumentException.class,
+              () -> new Employee("Frodo", "Baggins",
+                    "ring bearer", "Hobbit",-2));
+        assertEquals(JOB_YEARS_ERROR_MESSAGE, e.getMessage());
+    }
+
+    @Test
+    public void validEmployeeJobYearsZero() {
+        int expected = 0;
+        emp.setJobYears(0);
+        int actual = emp.getJobYears();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testEquals() {
+        Employee emp2 = new Employee("Frodo", "Baggins", "ring bearer", "Hobbit", 2);
+        assertEquals(emp, emp2);
+    }
 }
